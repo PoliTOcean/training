cc43f417906362966b6c325962cf015b71bd34fc
ALI HAGHIGHIPOUR <s261940@studenti.polito.it>
2020-10-30 21:53:19 +0100

    updated main.c / feature vision added

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void vision(void);
+
 int main(int argc, const char *argv[])
 {
     fprintf(stdout, "%s\n", "hello world!\n");
     return EXIT_SUCCESS;
+}
+
+void vision(void){
+
+    fprintf(stdout, "%s\n", "Feature vision added to the ROV");
+    return;
 }
